585e0ccee3727a919b0716243d40775564cc828a
Excoll <142539822+Excoll@users.noreply.github.com>
2025-12-10 23:17:33 +0200

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,37 @@
+ # Цілі числа (int)
+age = 19
+items_count = 42
+
+# Числа з плаваючою крапкою (float)
+temperature = 23.5
+pi_value = 3.14159
+
+# Рядки (str)
+user_name = "Maks"
+city_name = "Kyiv"
+
+# Логічні значення (bool)
+is_logged_in = True
+has_discount = False
+
+# Списки (list)
+favorite_numbers = [7, 13, 21]
+
+# Словники (dict)
+user_profile = {
+    "name": "Maks",
+    "age": 19,
+    "city": "Kyiv"
+}
+
+# Виведення всіх значень та їх типів
+print("age =", age, ", type:", type(age))
+print("items_count =", items_count, ", type:", type(items_count))
+print("temperature =", temperature, ", type:", type(temperature))
+print("pi_value =", pi_value, ", type:", type(pi_value))
+print("user_name =", user_name, ", type:", type(user_name))
+print("city_name =", city_name, ", type:", type(city_name))
+print("is_logged_in =", is_logged_in, ", type:", type(is_logged_in))
+print("has_discount =", has_discount, ", type:", type(has_discount))
+print("favorite_numbers =", favorite_numbers, ", type:", type(favorite_numbers))
+print("user_profile =", user_profile, ", type:", type(user_profile))
